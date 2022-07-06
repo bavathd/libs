0a1e713ab7667f756196e4b5c29f192720201d87
bavathd <dbavath@gmail.com>
2022-07-06 15:05:42 +0530

    correction

--- analogFrontEnd/analogFrontEnd.cpp
+++ analogFrontEnd/analogFrontEnd.cpp
@@ -13,12 +13,12 @@ analogFrontEnd::~analogFrontEnd(void)
 
 bool analogFrontEnd::begin(uint8_t i2c_address, TwoWire *wire, uint8_t sensor_id) {
     if(i2c_dev) {
-        delete i2c_dev
+        delete i2c_dev;
     }
     
     i2c_dev = new AdafruitI2CDevice(i2c_address, wire );
 
-    if(!i2c_dev->begin()) {
+    if(! i2c_dev -> begin()) {
         return false;
     }
 
